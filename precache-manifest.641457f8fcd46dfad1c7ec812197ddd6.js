4fa7c046f6ae54f46738238bcdaa3f9501727b04
Danilo Tomaccio <danilo.tomaccio@gmail.com>
2022-01-10 17:50:44 +0000

    Commit automatico

---
+++ precache-manifest.641457f8fcd46dfad1c7ec812197ddd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65ecc715621d9208817ef39f89565af",
+    "url": "/Explosion.mp3"
+  },
+  {
+    "revision": "008ecc45e232b3e5dbd8",
+    "url": "/css/app.c51244fc.css"
+  },
+  {
+    "revision": "dc97f34ade1b590e1688b6803b1a1f03",
+    "url": "/fonts/CabinSketch-Regular.dc97f34a.ttf"
+  },
+  {
+    "revision": "695cb73e2fe6d8719d9570ddc3aec2c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "008ecc45e232b3e5dbd8",
+    "url": "/js/app.31a3a4a9.js"
+  },
+  {
+    "revision": "9960ff9210f85e744155",
+    "url": "/js/chunk-vendors.2a1152a9.js"
+  },
+  {
+    "revision": "040e12787b4c3b520f20455516153a4d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff627f58b3c24a9e463850e82369993c.js"
+  "/precache-manifest.641457f8fcd46dfad1c7ec812197ddd6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "passalabomba"});
