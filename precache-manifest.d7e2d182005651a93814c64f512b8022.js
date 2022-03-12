6844917eb64f261403927a2e3e9dd74ac9b5a181
Danilo Tomaccio <danilo.tomaccio@gmail.com>
2022-03-12 23:03:16 +0000

    Pwa

---
+++ precache-manifest.d7e2d182005651a93814c64f512b8022.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02dc7bd36b158256f8e15d2e61e12282",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "f65ecc715621d9208817ef39f89565af",
+    "url": "/Explosion.mp3"
+  },
+  {
+    "revision": "7e112ece5065e7ec351b",
+    "url": "/css/app.11371890.css"
+  },
+  {
+    "revision": "dc97f34ade1b590e1688b6803b1a1f03",
+    "url": "/fonts/CabinSketch-Regular.dc97f34a.ttf"
+  },
+  {
+    "revision": "18a06f7567b78682d7e352b5dfc69b94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e112ece5065e7ec351b",
+    "url": "/js/app.3f6dea95.js"
+  },
+  {
+    "revision": "298339b46b729d31f341",
+    "url": "/js/chunk-vendors.e27855fc.js"
+  },
+  {
+    "revision": "f50b52e1cb6aff8575b691ef445ce48d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
